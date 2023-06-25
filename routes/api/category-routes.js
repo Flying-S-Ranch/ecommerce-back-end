1e9b48039d32759e3278c4ab31f3ea1d9e3dbd0b
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-25 16:59:59 -0500

    adds GET route for one category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,9 +17,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+// GET /api/categories/:id
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+    if (!catData) {
+      res.status(404).json({ message: 'No category found with that id'});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
