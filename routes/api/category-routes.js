2706909fb90ee679f63108c5a7d4bfff6f4f6493
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-25 18:51:26 -0500

    adds a draft of DEL route by category id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,8 +65,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+// DELETE /api/categories/:id
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCat = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedCat) {
+      res.status(404).json({ message: 'No category with this id' });
+      return;
+    }
+    res.status(200).json(deletedCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
