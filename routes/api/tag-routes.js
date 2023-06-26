7e983cf74ae041f889e8fa943dd16d86cdb3e435
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-25 20:21:34 -0500

    adds POST request to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,6 +39,12 @@ router.get('/:id', async (req, res) => {
 // POST /api/tags
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // PUT /api/tags/:id
