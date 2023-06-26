80409bc4db289cf67d2bf5d85d9fae089f3136cd
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-25 19:07:55 -0500

    adds a get route for all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+// GET /api/products
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      // the Sequelize documentation showed how to "include all associated models", please see README for link
+      include: [{ all: true }],
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
